f7d1bb13520374f1c8007499781661730a2980c0
abulmarch <abul.march@gmail.com>
2023-07-10 17:45:33 +0530

    merging splash splash_screen

--- lib/views/pages/login/splash_screen.dart
+++ lib/views/pages/login/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:need_in_choice/utils/constants.dart';
 import 'package:need_in_choice/views/pages/login/widgets/signin_modelsheet.dart';
 import 'package:need_in_choice/views/pages/login/widgets/start_button.dart';
@@ -44,49 +43,128 @@ class _SplashScreenState extends State<SplashScreen>
   
     final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        height: screenHeight,
-        width: screenWidth,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-              'assets/images/splash.png',
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              'search and buy anything with nic now',
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            kHeight20,
-            FutureBuilder(
-              future: Firebase.initializeApp(),
-              builder: (context, snapshot) {                
-                switch (snapshot.connectionState) {
-                  case ConnectionState.done:
-                    return StartButton(
-                      screenWidth: screenWidth,
-                      boldText: 'Get Started',
-                      lightText: ' Now',
-                      circle: kWhiteColor,
-                      button: kPrimaryColor,
-                      ontap: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(signUpScreen, (route) => false);
-                      },
-                      arrow: kPrimaryColor,
-                    );
-                  default :
-                    return const Center(child: CircularProgressIndicator(color: Colors.black,));
-                }
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: Stack(
+        children: [
+          Positioned(
+            top: -30,
+            left: 0,
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(0, _animation.value),
+                  child: Image.asset('assets/images/profile/Ellipse 95.png'),
+                );
               },
             ),
-            kHeight20
-          ],
-        ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2 - 75,
+            left: MediaQuery.of(context).size.width / 9 - 85,
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(0, _animation.value),
+                  child: const CircleAvatar(
+                    radius: 50,
+                    backgroundColor: kSecondaryColor,
+                  ),
+                );
+              },
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 4 - 45,
+            right: MediaQuery.of(context).size.width / 3 - 85,
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(0, -_animation.value),
+                  child: const CircleAvatar(
+                    radius: 25,
+                    backgroundColor: kPrimaryColor,
+                  ),
+                );
+              },
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2 - 75,
+            right: MediaQuery.of(context).size.width / 7 - 85,
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (BuildContext context, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(0, _animation.value),
+                  child: const CircleAvatar(
+                    radius: 40,
+                    backgroundColor: kLightGreyColor,
+                  ),
+                );
+              },
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2,
+            left: MediaQuery.of(context).size.height / 5 - 65,
+            child: Image.asset('assets/images/profile/nic.png'),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 1.25,
+            left: MediaQuery.of(context).size.height / 8 - 60,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'search and buy everything with nic now',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                kHeight20,
+                FutureBuilder(
+                  future: Firebase.initializeApp(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.done:
+                        return StartButton(
+                          screenWidth: screenWidth,
+                          boldText: 'Get Started',
+                          lightText: ' Now',
+                          circle: kWhiteColor,
+                          button: kPrimaryColor,
+                          ontap: () {
+                            showModalBottomSheet(
+                              backgroundColor: Colors.transparent,
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (context) => SingleChildScrollView(
+                                reverse: false,
+                                padding: EdgeInsets.only(
+                                  bottom:
+                                      MediaQuery.of(context).viewInsets.bottom,
+                                ),
+                                child: const SigninModalSheet(),
+                              ),
+                            );
+                          },
+                          arrow: kPrimaryColor,
+                        );
+                      default:
+                        return const Center(
+                            child: CircularProgressIndicator(
+                          color: Colors.black,
+                        ));
+                    }
+                  },
+                ),
+                kHeight20
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
